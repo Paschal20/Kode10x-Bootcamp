5825e9903eb5bd2614dd7dd722b6a9b8faa91051
Paschal Anaedu <170808501@live.unilag.edu.ng>
2025-06-28 09:23:30 +0100

    commit

--- signUp.js
+++ signUp.js
@@ -1,24 +1,39 @@
-const signUpForm = document.getElementById("signUpForm");
 const myname = document.getElementById("myname").value;
 const date = document.getElementById("date").value;
 const email = document.getElementById("email").value;
+const password = document.getElementById("password").value;
 
 signUpForm.addEventListener("submit", function (e) {
   e.preventDefault();
-  if (myname.trim() === "" || date.trim() === "" || email.trim() ==="") {
-    return alert("All field required");
-  } 
-    
+  if (myname === "" || date === "" || email === "" || password === "") {
+    alert("All field required");
+  }
+
   const userData = {
-      myname,
-      date,
-      email,
-    };
-    
-    localStorage.setItem("users", JSON.stringify(userData));
-    alert("sign up successfully");
-  
+    myname: myname,
+    date: date,
+    email: email,
+    password: password,
+  };
+
+  localStorage.setItem("users", JSON.stringify(userData));
+  alert("sign up successfully");
+
   setTimeout(() => {
     window.location.href = "signin.html";
   }, 1500);
 });
+
+
+
+function signIn() {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  const storedPassword = localStorage.getItem(email);
+  if (storedPassword === password) {
+    alert("Login successful!");
+  } else {
+    alert("Invalid username or password");
+  }
+}
